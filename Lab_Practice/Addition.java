66a5f5a4d691f256aae624c40429fb35ff4aea32
sivakumaran-29 <sivakumaran239@gmail.com>
2026-03-06 17:36:36 +0530

    Delete Lab_Practice/Addition.java

--- Lab_Practice/Addition.java
+++
@@ -1,8 +0,0 @@
-public class Addition {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int addition = a + b;
-        System.out.println("The addtion of num1 and num2 is:" + addition);
-       }
-}
